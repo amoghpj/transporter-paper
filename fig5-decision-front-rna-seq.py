44df0d12a9c487f31e6ae1de9347356c4ce5e49e
Jalihal <amogh.jalihal@gmail.com>
2021-07-05 16:15:30 -0400

    Update processing of RNAseq data
    1. Now process the raw counts file here
    2. Treat biological replicates correctly, not as technical replicates
    3. Log10 transform for scaling noise
    
    Plotting:
    Plot a single point for the PC comparison.
    Plot barplots using the data processed in this script, removes
    dependence on the R pipeline, except to aggregate the data.

--- fig5-decision-front-rna-seq.py
+++ fig5-decision-front-rna-seq.py
@@ -9,7 +9,7 @@ import matplotlib.pyplot as plt
 from matplotlib.patches import ConnectionPatch
 from sklearn.linear_model import LinearRegression
 from itertools import product
-from scipy.stats import pearsonr
+from scipy.stats import pearsonr, spearmanr
 from sklearn.decomposition import PCA
 import matplotlib
 font = {'size'   : 15}
@@ -149,29 +149,41 @@ pc = PCA(n_components=2)
 PC = pc.fit_transform(collectdf.values.T)
 
 #(gs[3:5,0:3])#(gs[1:,1:3])
-axpheno.set_xlabel("PC2")
-axphenotwin = axpheno.twinx()
-axpheno.set_yticks([])
+axpheno.set_xlabel("PC2 - On Fraction")
+axpheno.set_ylabel("PC1 - On Fraction")
+#axphenotwin = axpheno.twinx()
 #stmap ={"a28y":"k", "cd103":"g", "sh07":"r"}
 stmap ={"a28y":"#d72631", "cd103":"#12a4d9", "sh07":"#5c3c92"}
 colors = [stmap[c.split("-")[1]] for c in collectdf.columns]
 nut = [c.split("-")[0] for c in collectdf.columns]
 for n,c, x, y in zip(nut,colors,PC[:,1],PC[:,0]):
-    axphenotwin.plot(x,y,marker=plot_styles[n]['style'],markersize=plot_styles[n]['size'],c=c)
+    axpheno.plot(x,y,marker=plot_styles[n]['style'],markersize=plot_styles[n]['size'],c=c)
 #axphenotwin.scatter(PC[:,1],PC[:,0],c=colors,s=100)
 # for c, x, y in zip(collectdf.columns,PC[:,0],PC[:,1]):
 #     axphenotwin.annotate(c.split("-")[0].lower(),xy=(y,x),fontsize=16)
-axphenotwin.set_ylabel("PC1")
+axpheno.set_ylabel("PC1 - On Fraction")#
 axpheno.set_xlim(-2.5,3)
-axphenotwin.set_ylim(-3,3.5)
-axphenotwin.set_title("On Fraction")
+axpheno.set_ylim(-3,3.5)
+axpheno.set_title("On Fraction")
+# axphenotwin.set_title("On Fraction")
+# axphenotwin.set_ylim(-3,3.5)
+# axphenotwin.set_title("On Fraction")
 #### Transcriptional barplots
-expdf = pd.read_csv("./data/hxt-expression.csv")
-expdf = np.log10(np.power(2,expdf))
+# expdf = pd.read_csv("./data/hxt-expression.csv")
+# expdf = np.log10(np.power(2,expdf))
+countdf = pd.read_csv("./data/raw-counts/raw-counts-all.csv",index_col=0)
+countdf = countdf.loc[[ind for ind in list(countdf.index) if "HXT" in ind]]
+unique = list(set(["-".join(c.split("-")[:-1]) for c in countdf.columns]))
+expdf_col = pd.DataFrame(columns=unique,index=countdf.index)
+for u in unique:
+    coloi = [c for c in countdf.columns if c.startswith(u)]
+    expdf_col[u] = countdf[coloi].mean(axis='columns')
+expdf_col = expdf_col.replace(np.nan, 0.0)
+expdf = np.log10(expdf_col +1)
 
 # Now compute mean difference between rich and poor carbon sources...
-expdf['meandiff'] = expdf[['2-glc-cd103','2-fru-cd103','02-glc-cd103']].mean(axis=1) -\
-    expdf[["002-glc-cd103", "2-mal-cd103","2-raf-cd103","2-ace-cd103"]].mean(axis=1)
+expdf['meandiff'] = expdf[['2-glc-cd103','2-fru-cd103','02-glc-cd103']].median(axis=1) -\
+    expdf[["002-glc-cd103", "2-mal-cd103","2-raf-cd103","2-ace-cd103"]].median(axis=1)
 # ... sort those values and store the row order, which will be used to order the barplot below
 expdforder = list(expdf.sort_values(by='meandiff', axis=0,ascending = False).index)
 expdf = pd.melt(expdf.reset_index(), id_vars=['index'])
@@ -199,21 +211,31 @@ for i, (s, ax) in enumerate(zip(strains, axlist)):
     #2ax.legend('off')
 
 #### Transcriptional PCA
-rnaseqdf = pd.read_csv('./data/fig5-hxt-expression-pca.csv',index_col=0)
-rnaseqdf = rnaseqdf[~rnaseqdf.nutrient.str.contains("gal")]
+pc = PCA(n_components=2)
+expdf = np.log10(expdf_col + 1)
+expdf = expdf[[c for c in expdf.columns if "gal" not in c]]
+
 normmap = {"2-glc":"2glc", "02-glc":"02glc","002-glc":"002glc",
            "2-fru":"fru","2-ace":"ac","2-mal":"mal","2-raf":"raf"}
-rnaseqdf["normalized"] = [n.split("-")[2] + "-" + normmap["-".join(n.split("-")[:2])] for n in rnaseqdf.name]
-rnaseqdf["nutname"] = [normmap["-".join(n.split("-")[:2])] for n in rnaseqdf.name]
+
+rnaseq_pca = pc.fit_transform(expdf.T.values)
+
+rnaseqdf = pd.DataFrame({"PC1":rnaseq_pca[:,0],
+                         "PC2":rnaseq_pca[:,1],
+                         "strains":[c.split("-")[-1] for c in expdf.columns],
+                         "nutrient":[normmap["-".join(c.split("-")[:-1])] for c in expdf.columns],
+                         "normalized":[n.split("-")[2] + "-" + normmap["-".join(n.split("-")[:2])] for n in expdf.columns]})
+# rnaseqdf["normalized"] = [n.split("-")[2] + "-" + normmap["-".join(n.split("-")[:2])] for n in rnaseqdf.name]
+# rnaseqdf["nutname"] = [normmap["-".join(n.split("-")[:2])] for n in rnaseqdf.name]
 colors = [stmap[s] for s in rnaseqdf.strains]
-for n,c, x, y in zip(rnaseqdf.nutname, colors, rnaseqdf.PC1, rnaseqdf.PC2):
+for n,c, x, y in zip(rnaseqdf.nutrient, colors, rnaseqdf.PC1, rnaseqdf.PC2):
     axrna.plot(x,y,marker=plot_styles[n]['style'],c=c,markersize=plot_styles[n]['size'])
 #axrna.scatter(rnaseqdf.PC1,rnaseqdf.PC2,c=colors,s=100)
-for c, x, y in zip(rnaseqdf.nutname,rnaseqdf.PC1,rnaseqdf.PC2):
+for c, x, y in zip(rnaseqdf.nutrient,rnaseqdf.PC1,rnaseqdf.PC2):
     axrna.annotate("".join(c.split("-")[0:2]).lower(),xy=(x,y),fontsize=16,rotation=45)
 axrna.set_title("HXT expression")
-axrna.set_ylabel("PC2")
-axrna.set_xlabel("PC1")
+axrna.set_ylabel("PC2 - HXT expression")
+axrna.set_xlabel("PC1 - HXT expression")
 # axrna.set_xlim(-5,9)
 # axrna.set_ylim(-5,7)
 #### PC1 comparison
@@ -225,9 +247,12 @@ for s, n in product(strains, nutconditions):
 colors = [stmap[c.split("-")[1]] for c in collectdf.columns]
 for n,c, x, y in zip(nut, colors, ordered1, PC[:,0]):
     axcomp.plot(x,y,marker=plot_styles[n]['style'],c=c,markersize=plot_styles[n]['size'])
-pccompdf = pd.DataFrame({'pcpheno':PC[:,0],'pcrna':ordered1,'strains':[c.split("-")[1] for c in collectdf.columns]})
+pccompdf = pd.DataFrame({'pcpheno':PC[:,0],
+                         'pcrna':ordered1,
+                         'strains':[c.split("-")[1] for c in collectdf.columns],
+                         'nutrient':[c.split("-")[0] for c in collectdf.columns]})
 
-annotcoords={'sh07':(-2,2),'cd103':(0,-2),'a28y':(6,0)}
+annotcoords={'sh07':(-2,2),'cd103':(0,-2),'a28y':(3,0)}
 for s in strains:
     dfoi = pccompdf[pccompdf.strains == s]
     r, p = pearsonr(dfoi.pcpheno, dfoi.pcrna)
@@ -237,23 +262,26 @@ for s in strains:
 # for c, x, y in zip(collectdf.columns,ordered1,PC[:,0],):
 #     axcomp.annotate(c.split("-")[0].lower(),xy=(x,y),fontsize=16)
 axcomp.set_ylim(-3,3.5)
-axcomp.set_xticks([])
-axcomp.set_yticks([])
-
-for x1, y1,x2 in zip(PC[:,1],PC[:,0],ordered1):
-    xyA = (x2,y1)
-    xyB = (x1,y1)
-    con = ConnectionPatch(xyA=xyA, xyB=xyB, coordsA="data", coordsB="data",
-                      axesA=axcomp, axesB=axphenotwin, color="k",alpha=0.2)
-    con.set_in_layout(False)
-    axcomp.add_artist(con)
-for x1, y1,x2 in zip(ordered1, ordered2, PC[:,0]):
-    xyB = (x1,y1)
-    xyA = (x1,x2)
-    con = ConnectionPatch(xyA=xyA, xyB=xyB, coordsA="data", coordsB="data",
+axcomp.set_xlabel('PC1 - HXT expression')
+axcompt = axcomp.twinx()
+axcompt.set_ylabel('PC1 - On Fraction')
+# axcomp.set_xticks([])
+# axcomp.set_yticks([])
+ind = 9
+x1, y1, x2 = PC[ind,1],PC[ind,0],ordered1[ind]
+xyA = (x2,y1)
+xyB = (x1,y1)
+con = ConnectionPatch(xyA=xyA, xyB=xyB, coordsA="data", coordsB="data",
+                      axesA=axcomp, axesB=axpheno, color="k",alpha=0.2)
+con.set_in_layout(False)
+axcomp.add_artist(con)
+x1, y1,x2 = ordered1[ind], ordered2[ind], PC[ind,0]
+xyB = (x1,y1)
+xyA = (x1,x2)
+con = ConnectionPatch(xyA=xyA, xyB=xyB, coordsA="data", coordsB="data",
                       axesA=axcomp, axesB=axrna, color="k",alpha=0.2)
-    con.set_in_layout(False)
-    axcomp.add_artist(con)
+con.set_in_layout(False)
+axcomp.add_artist(con)
 
 plt.tight_layout()
 plt.savefig("decision-threshold-2.pdf",dpi=400)
